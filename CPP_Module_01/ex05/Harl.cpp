673180968e645ea955b9f3012e08d6742036a72a
cptfran <frankiszymon@gmail.com>
2024-05-08 09:05:08 +0200

    added permission file

--- CPP_Module_01/ex05/Harl.cpp
+++ CPP_Module_01/ex05/Harl.cpp
@@ -1,5 +1,11 @@
 #include "Harl.hpp"
 
-void Harl::complain(std::string level) {
-	void (Harl::*complaints[4])()
+void Harl::complain(std::string level)
+{
+	harl_func_t	harl_funcs[4] = {&Harl::debug, &Harl::info, &Harl::warning, &Harl::error};
+	std::string levels[4] = {"DEBUG", "INFO", "WARNING", "ERROR"};
+
+	for (size_t i = 0; i < 4; i += 1)
+		if (level == levels[i])
+			(this->*harl_funcs[i])();
 }
