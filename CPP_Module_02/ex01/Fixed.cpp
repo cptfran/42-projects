259972b7628e6b505c722dc7791f50df9ba3b0c8
cptfran <frankiszymon@gmail.com>
2024-05-08 13:31:42 +0200

    02

---
+++ CPP_Module_02/ex01/Fixed.cpp
@@ -0,0 +1,35 @@
+#include "Fixed.hpp"
+
+Fixed::Fixed() : fixedPointNum(0) { std::cout << "Default constructor called" << std::endl; }
+
+Fixed::Fixed(const Fixed &obj) {
+	std::cout << "Copy constructor called" << std::endl;
+	Fixed::operator=(obj);
+}
+
+Fixed &Fixed::operator=(const Fixed &obj) {
+	std::cout << "Copy assignment operator called" << std::endl;
+	if (this != &obj) {
+		fixedPointNum = obj.fixedPointNum;
+	}
+	return *this;
+}
+
+Fixed::~Fixed() { std::cout << "Destructor called" << std::endl; }
+
+Fixed::Fixed(const int val) : fixedPointNum(val << numOfFractionalBits) { std::cout << "Int constructor called" << std::endl; }
+
+Fixed::Fixed(const float val) {
+	fixedPointNum = roundf(val * (1 << numOfFractionalBits));
+	std::cout << "Float constructor called" << std::endl;
+}
+
+
+float Fixed::toFloat() const { return (float)fixedPointNum / (1 << numOfFractionalBits); }
+
+int Fixed::toInt() const { return fixedPointNum >> numOfFractionalBits; }
+
+std::ostream &operator<<(std::ostream &os, const Fixed &obj) {
+	os << obj.toFloat();
+	return os;
+}
---
+++ CPP_Module_02/ex01/Fixed.hpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <cmath>
+
+class Fixed {
+	public:
+		Fixed();
+		Fixed(const Fixed &obj);
+		Fixed &operator=(const Fixed &obj);
+		Fixed(const int val);
+		Fixed(const float val);
+		~Fixed();
+		float toFloat() const;
+		int toInt() const;
+		friend std::ostream& operator<<(std::ostream& os, const Fixed& obj);
+	private:
+		int fixedPointNum;
+		static const int numOfFractionalBits = 8;
+};
