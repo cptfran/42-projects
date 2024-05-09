5dc474ea2efd4864d175a3133ede53018ca52891
cptfran <frankiszymon@gmail.com>
2024-05-09 16:42:09 +0200

    ex03

---
+++ CPP_Module_02/ex02/Fixed.hpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <cmath>
+
+#define RED "\033[31m"
+#define LIGHT_GREEN "\033[92m"
+#define BLUE "\033[94m"
+#define YELLOW "\033[33m"
+#define GRAY "\033[90m"
+#define RESET "\033[0m"
+
+#define MSG_ASK LIGHT_GREEN "Which test? ('comparison', 'arithmetic', 'subject')\nUse 'exit' to exit the program" RESET
+#define ERR_WRONG_INPUT RED "Error: test not found" RESET
+#define ERR_DIV_BY_ZERO RED "\nError: divison by 0" RESET
+
+
+class Fixed {
+	public:
+		Fixed();
+		Fixed(const Fixed &obj);
+		Fixed &operator=(const Fixed &obj);
+		Fixed(const int val);
+		Fixed(const float val);
+		~Fixed();
+		float toFloat() const;
+		int toInt() const;
+		// 'friend' allows 'operator<<' to manipulate this class private members
+		friend std::ostream &operator<<(std::ostream &os, const Fixed &obj);
+		bool operator>(const Fixed &obj) const;
+		bool operator<(const Fixed &obj) const;
+		bool operator>=(const Fixed &obj) const;
+		bool operator<=(const Fixed &obj) const;
+		bool operator==(const Fixed &obj) const;
+		bool operator!=(const Fixed &obj) const;
+		Fixed operator+(const Fixed &obj) const;
+		Fixed operator-(const Fixed &obj) const;
+		Fixed operator*(const Fixed &obj) const;
+		Fixed operator/(const Fixed &obj) const;
+		
+	private:
+		int fixedPointNum;
+		static const int numOfFractionalBits = 8;
+};
---
+++ CPP_Module_02/ex02/main.cpp
@@ -0,0 +1,78 @@
+#include "Fixed.hpp"
+
+// void test_subject() {
+
+// 	Fixed a;
+// 	Fixed const b( Fixed( 5.05f ) * Fixed( 99.99 ) );
+
+// 	std::cout << a << std::endl;
+// 	std::cout << ++a << std::endl;
+// 	std::cout << a << std::endl;
+// 	std::cout << a++ << std::endl;
+// 	std::cout << a << std::endl;
+
+// 	std::cout << b << std::endl;
+
+// 	std::cout << Fixed::max( a, b ) << std::endl;
+
+// 	return 0;
+// }
+
+void test_arithmetic() {
+	Fixed a(2.5f);
+	Fixed b(3);
+	std::cout << "a(2.5) + b(3) ? " << a + b << std::endl;
+	std::cout << "a(2.5) - b(3) ? " << a - b << std::endl;
+	std::cout << "a(2.5) * b(3) ? " << a * b << std::endl;
+	std::cout << "a(2.5) / b(3) ? " << a / b << std::endl;
+}
+
+void test_comparison() {
+	Fixed a(100);
+	Fixed b(99.99f);
+	Fixed c(99.99f);
+	std::cout << "Is a(100) > b(99.99)? " << (a > b) << std::endl;
+	std::cout << "Is a(100) < b(99.99)? " << (a < b) << std::endl;
+	std::cout << "Is b(99.99) >= c(99.99)? " << (b >= c) << std::endl;
+	std::cout << "Is b(99.99) <= c(99.99)? " << (b <= c) << std::endl;
+	std::cout << "Is b(99.99) == c(99.99)? " << (b == c) << std::endl;
+	std::cout << "Is b(99.99) == a(100)? " << (b == a) << std::endl;
+	std::cout << "Is b(99.99) != a(100)? " << (b != a) << std::endl;
+}
+
+std::string parser(std::string input) {
+	while (1) {
+		std::cout << MSG_ASK << std::endl;
+		std::cout << GRAY;
+		std::getline(std::cin, input);
+		std::cout << RESET;
+		if (input != "exit" && input != "comparison" && input != "subject" && input != "arithmetic") {
+			std::cerr << ERR_WRONG_INPUT << std::endl;
+			continue;
+		}
+		return input;
+	}
+}
+
+void run_test(std::string input) {
+	if (input == "exit") {
+		std::cout << LIGHT_GREEN << "exit" << std::endl;
+		return ;
+	} else if (input == "comparison") {
+		test_comparison();
+		run_test(parser(input));
+	} else if (input == "arithmetic") {
+		test_arithmetic();
+		run_test(parser(input));
+	}
+	// else if (input == "subject") {
+	// 	test_subject();
+	// 	input = parser(input);
+	// }
+}
+
+int main() {
+	std::string input;
+	run_test(parser(input));
+	return 0;
+}
