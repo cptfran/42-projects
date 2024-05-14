aed64a977419f49261b87532da576e5e551a5a75
cptfran <frankiszymon@gmail.com>
2024-05-14 16:45:03 +0200

    started mod04

---
+++ CPP_Module_04/ex00/Msg.hpp
@@ -0,0 +1,11 @@
+#ifndef MSG_HPP
+#define MSG_HPP
+#include "Colors.hpp"
+
+#define MSG_DEFAULT_CONSTRUCTOR LIGHT_GREEN "Default Animal constructor called" RESET
+#define MSG_COPY_CONSTRUCTOR "Copy Animal constructor called" RESET
+#define MSG_COPY_ASSIGNMENT LIGHT_GREEN "Copy Animal assignment operator called" RESET
+#define MSG_DESTRUCTOR RED "Animal destructor called" RESET
+#define MSG_ANIMAL_DEFAULT_SOUND CYAN "Default sound" RESET
+
+#endif
