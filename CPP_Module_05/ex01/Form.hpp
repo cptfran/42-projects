9529cdcff29dcc9e1f8d4d360847d524ca37e187
cptfran <frankiszymon@gmail.com>
2024-05-21 09:57:47 +0200

    finished ex01

--- CPP_Module_05/ex01/Form.hpp
+++ CPP_Module_05/ex01/Form.hpp
@@ -1,8 +1,12 @@
-#include <string>
+#ifndef FORM_HPP
+#define FORM_HPP
+#include <iostream>
+#include "Bureaucrat.hpp"
 
 class Form {
 public:
     Form();
+    Form(const std::string& name, const int& gradeToSign, const int& gradeToExecute);
     Form(const Form& obj);
     Form& operator=(const Form& obj);
     ~Form();
@@ -12,9 +16,18 @@ public:
     class GradeTooLowException : public std::exception {
         const char* what() const throw();
     };
+    const std::string& getName() const;
+    bool getIsSigned() const;
+    const int& getGradeToSign() const;
+    const int& getGradeToExecute() const;
+    void beSigned(const Bureaucrat& obj);
 private:
     const std::string name;
     bool isSigned;
     const int gradeToSign;
     const int gradeToExecute;
 };
+
+std::ostream& operator<<(std::ostream& os, const Form& obj);
+
+#endif
