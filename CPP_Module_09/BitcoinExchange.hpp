19d78464a33ba91f2b65c0405b00955f98d11537
cptfran <frankiszymon@gmail.com>
2024-08-08 11:34:12 +0200

    bit init func created

--- CPP_Module_09/BitcoinExchange.hpp
+++ CPP_Module_09/BitcoinExchange.hpp
@@ -1,4 +1,6 @@
 #ifndef BITCOINEXCHANGE_HPP
 #define BITCOINEXCHANGE_HPP
 
+std::map<std::string, double> init_data_as_map_container_from_file(std::ifstream& file);
+
 #endif
--- CPP_Module_09/main.cpp
+++ CPP_Module_09/main.cpp
@@ -13,49 +13,35 @@ int main(int argc, char **argv)
         std::cerr << RED "Error: incorrect number of arguments\n" LIGHT_CYAN "Usage: ./btc <filename>" RESET << std::endl;
         return 2;
     }
-    std::ifstream data_file("data.csv");
-    if (!data_file)
+    std::ifstream dataFile("data.csv");
+    if (!dataFile)
     {
         std::cerr << RED "Error: unable to open the data file: 'data.csv'" RESET << std::endl;
         return 3;
     }
-    std::ifstream input_file(argv[1]);
-    if (!input_file)
+    std::ifstream inputFile(argv[1]);
+    if (!inputFile)
     {
         std::cerr << RED "Error: unable to open the input file: '" << argv[1] << "'" RESET << std::endl;
         return 4;
     }
-    // init map container for data file
-    std::map<std::string, double> dateExchangeRateBtcData;
-    std::string lineData;
-    std::getline(data_file, lineData);
-    while (std::getline(input_file, lineData))
+    std::map<std::string, double> dateValueBtcData = init_data_as_map_container_from_file(dataFile);
+    std::map<std::string, double> dateValueBtcInput = init_data_as_map_container_from_file(inputFile);
+    return 0;
+}
+
+std::map<std::string, double> init_data_as_map_container_from_file(std::ifstream& file)
+{
+    std::map<std::string, double> dateExchangeRateBtc;
+    std::string line;
+    std::getline(file, line);
+    while (std::getline(file, line))
     {
-        const std::size_t commaPosition = lineData.find(',');
+        const std::size_t commaPosition = line.find(',');
         if (commaPosition != std::string::npos)                                                                                          
         {
-            dateExchangeRateBtcData[lineData.substr(0, commaPosition)] = strtod(lineData.substr(commaPosition + 1).c_str(), NULL);
+            dateExchangeRateBtc[line.substr(0, commaPosition)] = strtod(line.substr(commaPosition + 1).c_str(), NULL);
         }
     }
-    for (std::map<std::string, double>::iterator i = dateExchangeRateBtcData.begin(); i != dateExchangeRateBtcData.end(); ++i)
-    {
-        std::cout << i->first + " => " << i->second << std::endl;
-    }
-    // init map container for input file
-    std::map<std::string, double> dateValueBtcInput;
-    std::string lineInput;
-    std::getline(input_file, lineInput);
-    while (std::getline(input_file, lineInput))
-    {
-        const std::size_t commaPosition = lineInput.find('|');
-        if (commaPosition != std::string::npos)
-        {
-            dateValueBtcInput[lineInput.substr(0, commaPosition)] = strtod(lineInput.substr(commaPosition + 1).c_str(), NULL);
-        }
-    }
-    for (std::map<std::string, double>::iterator i = dateValueBtcInput.begin(); i != dateValueBtcInput.end(); ++i)
-    {
-        std::cout << i->first + " => " << i->second << std::endl;
-    }
-    return 0;
+    return dateExchangeRateBtc;
 }
